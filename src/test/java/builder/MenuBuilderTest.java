976b8aa38d4eaf513ce6204e31cfc333d328d555
Daniel Vilà <danivila.dev@gmail.com>
2026-03-11 20:07:47 +0100

    modify pom adding junit dependencies

---
+++ src/test/java/builder/MenuBuilderTest.java
@@ -0,0 +1,56 @@
+package builder;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuBuilderTest {
+
+    @Test
+    void testExecutiveMenu() {
+        Menu menu = new MenuBuilder()
+                .withStarter("Amanida Mediterrània")
+                .isVegan()
+                .isGlutenFree()
+                .withMainCourse("Filet de Vedella")
+                .withSupplement("Guarnició extra")
+                .withDessert("Mousse de xocolata")
+                .withDrink("Vi Negre")
+                .build();
+
+        assertEquals("Amanida Mediterrània", menu.getStarter());
+        assertEquals("Filet de Vedella", menu.getMainCourse());
+        assertEquals("Mousse de xocolata", menu.getDessert());
+        assertEquals("Vi Negre", menu.getDrink());
+        assertTrue(menu.isVegan());
+        assertTrue(menu.isGlutenFree());
+    }
+
+    @Test
+    void testKidsMenu() {
+        Menu menu = new MenuBuilder()
+                .withMainCourse("Macarrons amb tomàquet")
+                .withDessert("Gelat de Vainilla")
+                .withDrink("Suc de taronja")
+                .build();
+
+        assertNull(menu.getStarter());
+        assertEquals("Macarrons amb tomàquet", menu.getMainCourse());
+        assertEquals("Gelat de Vainilla", menu.getDessert());
+        assertEquals("Suc de taronja", menu.getDrink());
+    }
+
+    @Test
+    void testHalfMenu() {
+        Menu menu = new MenuBuilder()
+                .withMainCourse("Risotto de bolets")
+                .isVegan()
+                .withCoffee("Espresso")
+                .withDrink("Aigua")
+                .build();
+
+        assertNull(menu.getStarter());
+        assertNull(menu.getDessert());
+        assertEquals("Espresso", menu.getCoffe());
+        assertTrue(menu.isVegan());
+    }
+}
